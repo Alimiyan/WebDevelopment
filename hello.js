37114cd9cf612d5ef1d1c837d987fa5c5df9c4e2
Alimiyan <alimiyan123@gmail.com>
2021-06-18 23:55:26 -0700

    initial commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+var num=10
+var name= "Hello world"
+var name1= new String("hello")
+k=20
+n=10
+var sum=k+n
+console.log(sum)
+console.log(k)
+console.log(name.length)
+console.log("num = " + num)
