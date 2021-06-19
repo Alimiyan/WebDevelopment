ddb7bf649cee93d4b912053b86b1ce3a081732e7
Alimiyan <alimiyan123@gmail.com>
2021-06-19 00:46:58 -0700

    addded functions

---
+++ Day 1/function.js
@@ -0,0 +1,37 @@
+function sample(){
+    console.log("Hello World")
+    return 100
+}
+
+
+val=sample()        ///
+console.log(val)  ///  console.log(hello())
+
+function sa(num1,num2){
+    sum=num1+num2
+    console.log(sum)
+}
+sa(10,20)
+//function with argument and without return value
+//---------------------------------------
+ 
+function ab(a,b){
+    return sum=a+b
+}
+console.log(ab(10,50))
+//function with argument and return value
+//---------------------------------------
+
+function ac(){
+    sum=14+10
+    console.log(sum)
+}
+ac()
+//function without argument and without return value
+//--------------------------------
+
+function tu(){
+    return sum=14+15
+}
+console.log(tu())
+//function without argument and with return value
