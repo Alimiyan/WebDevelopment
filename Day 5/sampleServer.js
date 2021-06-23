42dd65e8acd53124b41ca6333756797a2427cf65
Alimiyan <alimiyan123@gmail.com>
2021-06-23 04:32:20 -0700

    added frontend

--- Day 5/sampleServer.js
+++ Day 5/sampleServer.js
@@ -1,8 +1,16 @@
 // server creation
 var http=require('http') //importing http module
+var fs=require('fs') //importing file system module, to acces system files
+
 http.createServer(function(req,res){
-    res.write("hello world") // content to display
-    res.end()   //end of request
-}).listen(7000)
+    
+    //passing the file
+    fs.readFile('Day 5/samplefrontend.html',function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'}) //error code, type of file to display
+        res.write(data) // content to display
+        res.end()   //end of request
+    })
+   
+}).listen(7000)//portnumber
 
     
