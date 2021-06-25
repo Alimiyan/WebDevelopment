893d087b59b61bc00509c4c2ee1650043a3393c2
Alimiyan <alimiyan123@gmail.com>
2021-06-25 04:52:21 -0700

    added some codes

--- Day 6/express_sam.js
+++ Day 6/express_sam.js
@@ -1,8 +1,16 @@
 const express= require('express')
 const path = require('path')
 const app = express()
+
+//for body parsing,,getting the values
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 //works between req and res
 app.use(function(req,res,next){
+    console.log("----------------------")
     console.log('working started')
     next() //didn't work the upcoming codes without calling this fun
 })
@@ -20,11 +28,17 @@ app.get('/about',(req,res,next)=>{
     next()
 })
 
-app.get('/signup',(req,res)=>{
+app.get('/signup',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'signup.html'))
+    console.log("on signup")
+    next()
 })
-app.post('/signup',(req,res)=>{
-    res.send("Successfully Registered")
+app.post('/signup',(req,res,next)=>{
+    var name=req.body.FName
+    res.send("<h2>Hello there "+name+ "<h2>" )
+   // console.log(name)
+    console.log("on completed reg")
+    next()
 })
 app.use((req,res)=>{
     console.log("working ended")
