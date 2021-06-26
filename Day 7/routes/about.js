e95fba4bb1967de1b53d19017b1590079e4b1fb9
Alimiyan <alimiyan123@gmail.com>
2021-06-25 23:04:45 -0700

    moded

--- Day 7/routes/about.js
+++ Day 7/routes/about.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET  about page. */
 router.get('/', function(req, res, next) {
-  res.send('This is the about page');
+  res.render('about', {value: 'sample'});
 });
 
 module.exports = router;
