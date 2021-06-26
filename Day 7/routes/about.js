7ed027a3057de17cc2dcac304de3c57ee9496815
Alimiyan <alimiyan123@gmail.com>
2021-06-25 22:47:10 -0700

    made some changes

--- Day 7/routes/about.js
+++ Day 7/routes/about.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: "Alimiyan" });
+  res.send('This is the about page');
 });
 
 module.exports = router;
